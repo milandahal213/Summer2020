da1e6a0d66972342161fa114b81a886f57c9bfe1
chrisbuerginrogers <crogers@tufts.edu>
2020-06-02 14:59:49 -0400

    AddedMelissa

---
+++ LabVIEWCode/Subs/pySerial/RogersSerial.py
@@ -0,0 +1,32 @@
+import serial,time
+
+ser = None
+
+def InitSerial(port,baudrate,timeout):
+    global ser
+
+    reply = 'None'
+    try:
+        ser = serial.Serial(port,baudrate = baudrate,timeout = timeout)  # open serial port
+    except Exception as e:
+        reply = e
+    return reply
+
+def N_Serial():
+    global ser
+    n = ser.inWaiting()
+    return str(n)
+
+def WriteSerial(text):
+    global ser
+    send = bytes(text, 'ascii')
+    n = ser.write(send)
+    return str(n)
+
+def ReadSerial(len):
+    global ser
+    return str(ser.read(len))
+
+def CloseSerial():
+    global ser
+    ser.close()             # close port
